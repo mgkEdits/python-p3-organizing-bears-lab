cb87a4d0dcf3fa44d1444c3f557ce4834a904a62
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-04 15:02:19 +0300

    first sln attempt

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES 
+('Mr. Chocolate', 5, 'M', 'Brown', 'Playful', 1),
+('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+('Tabitha', 6, 'F', 'Golden', 'Calm', 1),
+('Sergeant Brown', 8, 'M', 'Dark Brown', 'Disciplined', 1),
+('Melissa', 4, 'F', 'Honey', 'Sweet', 1),
+('Grinch', 10, 'M', 'Green', 'Mischevious', 1),
+('Wendy', 7, 'F', 'White', 'Gentle', 1),
+(NULL, 2, 'F', 'Cinnamon', 'Curious', 1);
